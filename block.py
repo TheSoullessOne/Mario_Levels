44b37fd1250f91912345db80d01f9017dfea358f
taintedbones <kjust2@saddleback.edu>
2019-10-14 22:20:48 -0700

    Started base class and inherited classes for blocks

---
+++ block.py
@@ -0,0 +1,40 @@
+from pygame.sprite import Sprite
+
+
+class Block(Sprite):
+    def __init__(self, screen, settings):
+        super(Block, self).__init__()
+        self.screen = screen
+        self.settings = settings
+        self.width = 10
+        self.height = self.width
+        self.moving_up = False
+        self.moving_down = False
+        # self.image
+        # self.rect = self.image.get_rect()
+        # self.center = self.rect.centerx
+
+    def blit_me(self, screen):
+        # screen.blit(self.image, self.rect)
+        pass
+
+    def update(self):
+        # if self.moving_up:
+        #     self.center += 1
+        # elif self.moving_down:
+        #     self.center -= 1
+        pass
+
+
+class ItemBlock(Block):
+    def __init__(self, item, screen, settings):
+        super().__init__(screen, settings)
+        # self.image =
+        self.item = item
+        self.activated = False
+
+
+class UsedBlock(Block):
+    def __init__(self, screen, settings):
+        super().__init__(screen, settings)
+        # self.image =

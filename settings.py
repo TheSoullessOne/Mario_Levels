d6640a63ff504ce349cf59e046c7e1763706ccee
William Au <WilliamAuCS@gmail.com>
2019-10-18 11:31:47 -0700

    Fixed jump issue

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (156, 219, 255)
 
-        self.max_jump_height = 200
+        self.max_jump_height = 300
         self.jmp_speed = float(1)

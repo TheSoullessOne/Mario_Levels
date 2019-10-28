75ce795d492bda67fd9ca50cc1583376f53b60ae
Kierstyn Just <kierstyn.erin@gmail.com>
2019-10-28 14:41:14 -0700

    Added update function for background movement

--- background.py
+++ background.py
@@ -7,9 +7,12 @@ class Background(Sprite):
         super().__init__()
         self.screen = screen
         self.settings = settings
-        screen_rect = screen.get_rect()
 
         self.image = pygame.image.load('Images/Backgrounds/1-1-bg.png')
         self.rect = self.image.get_rect()
         self.rect.left = screen.get_rect().left
         self.rect.top = screen.get_rect().top
+
+    def update(self):
+        self.rect.left -= 1
+        # Change speed to default movement speed of mario from settings

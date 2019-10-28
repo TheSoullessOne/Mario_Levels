55799b6d36c9033dd8e6a0802defdfb6282dcdfb
Kierstyn Just <kierstyn.erin@gmail.com>
2019-10-28 14:49:58 -0700

    Added function to initialize background placement to start

--- background.py
+++ background.py
@@ -7,6 +7,7 @@ class Background(Sprite):
         super().__init__()
         self.screen = screen
         self.settings = settings
+        self.screen_rect = screen.get_rect()
 
         self.image = pygame.image.load('Images/Backgrounds/1-1-bg.png')
         self.rect = self.image.get_rect()
@@ -16,3 +17,7 @@ class Background(Sprite):
     def update(self):
         self.rect.left -= 1
         # Change speed to default movement speed of mario from settings
+
+    def initialize_bg(self):
+        self.rect.left = self.screen_rect.left
+        self.rect.top = self.screen_rect.top
